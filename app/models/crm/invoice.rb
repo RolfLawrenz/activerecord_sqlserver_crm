902229a40d4d48aa0076216fd1b5c374fb6a5f3c
Rolf Lawrenz <rolf.lawrenz@gmail.com>
2016-06-30 12:11:32 -0600

    Added currency to invoice

--- app/models/crm/invoice.rb
+++ app/models/crm/invoice.rb
@@ -6,6 +6,7 @@ module Crm
     belongs_to :account, foreign_key: 'AccountId', crm_key: 'customerid_account'
     belongs_to :contact, foreign_key: 'ContactId', crm_key: 'customerid_contact'
     belongs_to :price_list, foreign_key: 'PriceLevelId', crm_key: 'pricelevelid'
+    belongs_to :currency, foreign_key: 'TransactionCurrencyId', crm_key: 'transactioncurrencyid'
 
     has_many :invoice_products, foreign_key: 'InvoiceId'
     has_many :notes, foreign_key: 'ObjectId'
