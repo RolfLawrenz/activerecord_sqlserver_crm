9ff24bf0db127678df25b61847169a5de54aa22b
Rolf Lawrenz <rolf.lawrenz@gmail.com>
2016-12-08 12:42:47 -0700

    Fixed Opportunity fields

--- app/models/crm/opportunity.rb
+++ app/models/crm/opportunity.rb
@@ -4,7 +4,10 @@ module Crm
     self.primary_key = "OpportunityId"
 
     belongs_to :campaign, foreign_key: 'CampaignId', crm_key: 'campaignid'
-    belongs_to :contact, foreign_key: 'contactid', crm_key: 'parentcontactid'
+    belongs_to :account, foreign_key: 'AccountId', crm_key: 'customerid_account'
+    belongs_to :contact, foreign_key: 'ContactId', crm_key: 'customerid_contact'
+    belongs_to :parent_account, foreign_key: 'ParentAccountId', crm_key: 'parentaccountid', class_name: 'Crm::Account'
+    belongs_to :parent_contact, foreign_key: 'ParentContactId', crm_key: 'parentcontactid', class_name: 'Crm::Contact'
 
     has_many :invoices, foreign_key: 'OpportunityId'
     has_many :notes, foreign_key: 'ObjectId'
